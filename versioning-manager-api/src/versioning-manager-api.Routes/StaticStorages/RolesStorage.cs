54ae1ac835b6c4769577ba876b1b8917835236b5
EBCEYS <kuznetsovalexeyu4eba@inbox.ru>
2025-08-24 14:29:02 +0300

    try fix client building

--- versioning-manager-api/src/versioning-manager-api.Routes/StaticStorages/RolesStorage.cs
+++ versioning-manager-api/src/versioning-manager-api.Routes/StaticStorages/RolesStorage.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace versioning_manager_api.Routes.StaticStorages;
 
 public static class RolesStorage
